bd99aa7beb0d662aff20df21f13518469ca000be
Vikrant Malla <vikrantmalla999@gmail.com>
2023-06-02 21:50:47 +0545

    feat: setup a json-server and asyncThunk

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
+import axios from 'axios';
 
 // action name constants
 const increment = "increment";
 const decrement = "decrement";
 const incrementByAmount = "incrementByAmount";
+const incementByApi = "incementByApi";
 
 // Store
 // Create a Redux store holding the state of your app.
 // Its API is { subscribe, dispatch, getState }.
-let store = createStore(reducer, applyMiddleware(logger.default));
+let store = createStore(reducer, applyMiddleware(logger.default, thunk.default));
 
 // Reducer
 // function that takes a current state value
@@ -26,6 +29,8 @@ function reducer(state = { amount: 1 }, action) {
       return { amount: state.amount - 1 };
     case incrementByAmount:
       return { amount: state.amount + action.payload };
+    case incementByApi:
+      return { amount: action.payload };
     default:
       return state;
   }
@@ -54,6 +59,19 @@ function incByAmt(value) {
   return { type: incrementByAmount, payload: value };
 }
 
+function initData(value) {
+  return {type: incementByApi, payload: value}
+}
+
+// Async Action creator
+function getData(id) {
+  return async(dispatch, getState) => {
+  // Async API call
+  const {data} = await axios.get(`http://localhost:3000/accounts/${id}`)
+  dispatch(initData(data.amount))
+  }
+}
+
 // Action
 // after creating dispatch add to reducer function
 // store.dispatch({ type: 'increment' })
@@ -62,5 +80,12 @@ function incByAmt(value) {
 setInterval(() => {
   // store.dispatch(inc());
   // store.dispatch(dec());
-  store.dispatch(incByAmt(5));
-}, 2000);
+  // store.dispatch(incByAmt(5))
+}, 5000);
+
+store.dispatch(getData(3));
+
+
+// Note - Redux Thunk is a middleware that lets you call action creators that return a function instead of an action object.
+// This allows for delayed actions, including working with promises.
+// A thunk function takes dispatch (and getState) as parameters
